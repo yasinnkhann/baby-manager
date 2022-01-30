1e1d278aea46d0403c108402ab4a9b6d4a970b00
Ryan Olea <ryanaolea@gmail.com>
2022-01-29 17:06:06 -0800

    add baby, nav, footer skeleton

--- pages/addBaby.js
+++ pages/addBaby.js
@@ -19,57 +19,64 @@ export default function AddBaby() {
   const [date, setDate] = useState(null);
 
   return (
-    <main className="font-['Rubik'] h-screen mx-auto w-1/2 flex flex-col justify-center">
-      <h1 className='font-medium self-center text-4xl'>Add A Baby</h1>
-      <ChildCareIcon className='self-center' sx={{ fontSize: 140 }} />
-      <FormControl sx={{ m: 1, minWidth: 80 }} className='flex flex-col'>
-        <TextField className='py-4 pb-10' id='name' label='Name' variant='standard'></TextField>
+    <div className='h-screen'>
+      <main className="font-['Rubik'] h-full mx-auto w-1/2 flex flex-col justify-center px-4">
+        <h1 className='font-medium self-center text-xl md:text-4xl lg:text-5xl'>Add A Baby</h1>
+        <ChildCareIcon className='self-center sm:text-5xl lg:text-6xl xl:text-9xl' />
+        <FormControl sx={{ m: 1, minWidth: 80 }} className='flex flex-col'>
+          <TextField
+            className='py-4 pb-10'
+            id='name'
+            label='Name'
+            variant='standard'
+          ></TextField>
 
-        <LocalizationProvider dateAdapter={AdapterDateFns}>
-          <DatePicker
-            label='Birthday'
-            value={date}
-            onChange={newValue => {
-              setDate(newValue);
-            }}
-            renderInput={params => <TextField {...params} />}
-          />
-        </LocalizationProvider>
+          <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <DatePicker
+              label='Birthday'
+              value={date}
+              onChange={newValue => {
+                setDate(newValue);
+              }}
+              renderInput={params => <TextField {...params} />}
+            />
+          </LocalizationProvider>
 
-        <div className='flex flex-row space-x-8 justify-center pt-5 '>
-          <IconButton color='primary' aria-label='upload picture' component='span'>
-            <FemaleIcon />
-          </IconButton>
-          <IconButton color='primary' aria-label='upload picture' component='span'>
-            <MaleIcon />
-          </IconButton>
-        </div>
+          <div className='flex flex-row space-x-8 justify-center pt-5 '>
+            <IconButton color='primary' aria-label='upload picture' component='span'>
+              <FemaleIcon />
+            </IconButton>
+            <IconButton color='primary' aria-label='upload picture' component='span'>
+              <MaleIcon />
+            </IconButton>
+          </div>
 
-        <Slider
-          className='py-8'
-          valueLabelDisplay='auto'
-          min={0}
-          max={25}
-          step={0.1}
-          onChange={e => setWeight(e.target.value)}
-        ></Slider>
+          <Slider
+            className='py-8'
+            valueLabelDisplay='auto'
+            min={0}
+            max={25}
+            step={0.1}
+            onChange={e => setWeight(e.target.value)}
+          ></Slider>
 
-        <FormLabel id='demo-row-radio-buttons-group-label'>Weight Type</FormLabel>
-        <Select
-          aria-labelledby='demo-row-radio-buttons-group-label'
-          id='weight-type-select'
-          value={weightType}
-          label='Weight Type'
-          onChange={e => setWeightType(e.target.value)}
-        >
-          <MenuItem key='lb' value='lb'>
-            lb
-          </MenuItem>
-          <MenuItem key='kg' value='kg'>
-            kg
-          </MenuItem>
-        </Select>
-      </FormControl>
-    </main>
+          <FormLabel id='demo-row-radio-buttons-group-label'>Weight Type</FormLabel>
+          <Select
+            aria-labelledby='demo-row-radio-buttons-group-label'
+            id='weight-type-select'
+            value={weightType}
+            label='Weight Type'
+            onChange={e => setWeightType(e.target.value)}
+          >
+            <MenuItem key='lb' value='lb'>
+              lb
+            </MenuItem>
+            <MenuItem key='kg' value='kg'>
+              kg
+            </MenuItem>
+          </Select>
+        </FormControl>
+      </main>
+    </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,11 @@ module.exports = {
     extend: {
       screens: {
         '3xl': '2000px',
+        '2xl': '1536px',
+        xl: '1280px',
+        lg: '1024px',
+        md: '768',
+        sm: '640',
       },
     },
     fontFamily: {
