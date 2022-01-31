ca7c17f2d15c4bd66a4116558f56ed880b997a67
Daniel-Ghaly <ghalydan@gmail.com>
2022-01-31 15:39:41 -0800

    async fix for error I had

--- pages/calendar.js
+++ pages/calendar.js
@@ -1,16 +1,43 @@
 import WeeklyView from '../components/Calendar/WeeklyView.js';
 import ListView from '../components/Calendar/ListView.js';
 import { db } from '../firebaseConfig.js';
-
-import { collection, getDocs } from 'firebase/firestore';
-
-const querySnapshot = await getDocs(collection(db, 'feedingEvents'));
-querySnapshot.forEach(doc => {
-  // doc.data() is never undefined for query doc snapshots
-  console.log(doc.id, ' => ', doc.data());
-});
+import { useEffect } from 'react';
+import { collection, getDocs, doc } from 'firebase/firestore';
+import { auth } from '../firebaseConfig.js';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { query, where } from 'firebase/firestore';
 
 function Calendar() {
+  //   const [user, loading, error] = useAuthState(auth);
+  //   // var getCurrentUserID = async () => {
+  //   //   if (user) {
+  //   //     console.log('userID: ', user.uid);
+  //   //   }
+  //   // }
+
+  //   var getCurrentUserBabies = async () => {
+  //     if (user) {
+  //       const babies = collection(db, "users", user.uid, "babies");
+  //       const querySnapshot = await getDocs(babies);
+  //       querySnapshot.forEach(async (doc) => {
+  //         const feedingEvents = collection(db, "users", user.uid, "babies", doc.id);
+  //         const querySnapshot = await getDocs(baby);
+  //       });
+  //     }
+  //   }
+
+  //   // var getUsers = async () => {
+  //   //   const querySnapshot = await getDocs(query(collection(db, "users"), where("email", "==", "test@email.com")));
+  //   //   querySnapshot.forEach((doc) => {
+  //   //     console.log((doc.data()));
+  //   //   });
+  //   // }
+
+  //   useEffect(()=> {
+  //     // getUsers();
+  //     // getCurrentUserID();
+  //     getCurrentUserBabies();
+  //   })
   return (
     <>
       <WeeklyView />
