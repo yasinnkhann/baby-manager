d6f3979b794d67bd4a7f2606e5b760fabb52b5aa
Derek Wing <derekwingg@gmail.com>
2022-01-31 17:47:09 -0600

    Creates invitation subcollection in DB

--- pages/api/invite.js
+++ pages/api/invite.js
@@ -1,35 +1,42 @@
 const nodemailer = require('nodemailer');
-const crypto = require('crypto');
+// const crypto = require('crypto');
 
 export default function inviteHandler(req, res) {
   // Create random token
   // Create firebase query to store hash,main user id/email, provided email to invite
-  var token = crypto.randomBytes(8).toString('hex');
-  const INVITE_LINK = `http://localhost:3000/?=${token}`;
+  try {
+    // console.log('req.body:', req.body);
+    const token = req.body.token;
+    // var token = crypto.randomBytes(8).toString('hex');
+    const INVITE_LINK = `http://localhost:3000/?=${token}`;
 
-  const email = req.body;
-  let transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: process.env.NEXT_PUBLIC_SERVER_EMAIL,
-      pass: process.env.NEXT_PUBLIC_SERVER_EMAIL_PASSWORD,
-    },
-  });
+    const email = req.body.email;
+    let transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.NEXT_PUBLIC_SERVER_EMAIL,
+        pass: process.env.NEXT_PUBLIC_SERVER_EMAIL_PASSWORD,
+      },
+    });
 
-  let mailOptions = {
-    from: process.env.NEXT_PUBLIC_SERVER_EMAIL,
-    to: email,
-    subject: 'Team Flareon is the best!!!',
-    text: `You are invited to manage {users name}'s babies. Click the following link to log in or sign up to manage his/her babies! \n\n${INVITE_LINK}. \n\nOnly log in through this link to gain access to {users name}'s babies.`,
-  };
+    let mailOptions = {
+      from: process.env.NEXT_PUBLIC_SERVER_EMAIL,
+      to: email,
+      subject: 'Team Flareon is the best!!!',
+      text: `You are invited to manage {users name}'s babies. Click the following link to log in or sign up to manage his/her babies! \n\n${INVITE_LINK}. \n\nOnly log in through this link to gain access to {users name}'s babies.`,
+    };
 
-  let data = transporter.sendMail(mailOptions, (err, data) => {
-    if (err) {
-      console.log('Error:', err);
-      res.status(400).json({ msg: 'Error sending email:', err });
-    } else {
-      console.log('Email sent successfully');
-      res.status(201).json({ msg: 'Email sent successfully.', data });
-    }
-  });
+    let data = transporter.sendMail(mailOptions, (err, data) => {
+      if (err) {
+        console.log('Error:', err);
+        res.status(400).end();
+      } else {
+        console.log('Email sent successfully');
+        res.status(201).end();
+      }
+    });
+  } catch (err) {
+    console.log('Caught Error:', err);
+    res.status(400).end();
+  }
 }
--- pages/user.js
+++ pages/user.js
@@ -1,8 +1,37 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth, db } from '../firebaseConfig.js';
+import { collection, doc, setDoc, getDoc, updateDoc, addDoc } from 'firebase/firestore';
 import Link from 'next/link';
+const crypto = require('crypto');
 
 export default function User() {
   const [email, setEmail] = useState('');
+  const [user, loading, error] = useAuthState(auth);
+
+  useEffect(() => {
+    console.log(user);
+  }, [user]);
+
+  const getUserInfo = async () => {
+    const userRef = doc(db, 'users', user.uid);
+    const docSnap = await getDoc(userRef);
+
+    if (docSnap.exists()) {
+      console.log('Document data:', docSnap.data());
+    } else {
+      // doc.data() will be undefined in this case
+      console.log('No such document!');
+    }
+  };
+
+  const createInvitation = async token => {
+    const docRef = await addDoc(collection(db, 'users', 'testuser123', 'invitations'), {
+      emailSentTo: email,
+      token: token,
+      accepted: false,
+    });
+  };
 
   const handleChange = () => {
     setEmail(event.target.value);
@@ -10,27 +39,29 @@ export default function User() {
 
   const handleInviteButton = async e => {
     e.preventDefault();
-
-    const response = await fetch('/api/invite', {
+    const token = crypto.randomBytes(8).toString('hex');
+    const body = {
+      email: email,
+      token: token,
+    };
+    createInvitation(token);
+    const res = await fetch('/api/invite', {
       method: 'POST',
-      body: JSON.stringify(email),
+      body: JSON.stringify(body),
       headers: {
         'Content-Type': 'application/json',
       },
     });
-    const data = await response.json();
-    console.log('data from api:', data);
     alert('Invite Sent!');
   };
 
-  return (
+  return user ? (
     <article>
       <section className='h-screen my-[10%]'>
         <div>
-          <div>First Name</div>
-          <div>Last Name</div>
-          <div>Email</div>
-          <div>Phone Number</div>
+          <div>Name: {user.displayName}</div>
+          <div>Email: {user.email}</div>
+          <div>Phone Number: {user.phoneNumber}</div>
         </div>
         <div>
           <div>[List of names that are authorized to manage your baby details]</div>
@@ -40,9 +71,10 @@ export default function User() {
               <input type='text' onChange={handleChange}></input>
               <button type='submit'>Invite User</button>
             </form>
+            <button onClick={getUserInfo}>Print User Info</button>
             {/* <Link href='/'>
-            <a>Invitation Link</a>
-          </Link> */}
+              <a>Invitation Link</a>
+            </Link> */}
             <div>
               <button>Sign Out Button</button>
             </div>
@@ -50,5 +82,5 @@ export default function User() {
         </div>
       </section>
     </article>
-  );
+  ) : null;
 }
