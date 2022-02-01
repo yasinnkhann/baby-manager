f2db4c4ab7510cd90936652fc8ff4f58f9e3cdb3
Alissa Luk <alissaluk1995@gmail.com>
2022-01-31 17:16:01 -0800

    changed the google maps library and added markers and markers

--- components/ChangingRoom/GoogleMap.js
+++ components/ChangingRoom/GoogleMap.js
@@ -1,26 +1,82 @@
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
-import { compose, withProps } from 'recompose';
-import { InfoBox } from 'react-google-maps/lib/components/addons/InfoBox';
+import React, { Component, useState } from 'react';
+import {
+  GoogleMap,
+  LoadScript,
+  Marker,
+  InfoWindow,
+  StandaloneSearchBox,
+} from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '100%',
+  height: '500px',
+};
 
 const API_KEY = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API;
+const libraries = ['places'];
 
-export const MyMapComponent = compose(
-  withProps({
-    googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&v=3.exp&libraries=geometry,drawing,places`,
-    loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `500px` }} />,
-    mapElement: <div style={{ height: `100%`, width: '100%' }} />,
-  }),
-  withScriptjs,
-  withGoogleMap
-)(props => (
-  <GoogleMap
-    defaultZoom={8}
-    defaultCenter={props.location}
-    center={props.location}
-    clickableIcons={true}
-  >
-    {props.isMarkerShown && <Marker position={{ lat: 47.397, lng: -120.644 }} />}
-    <Marker position={{ lat: 47.397, lng: -121.644 }} />
-  </GoogleMap>
-));
+function MyComponents({ center, zoom }) {
+  const [searchBox, setSearchBox] = useState(null);
+  const onLoad = ref => {
+    searchBox = ref;
+  };
+  const onPlacesChanged = () => setSearchBox(searchBox.getPlaces());
+
+  return (
+    <LoadScript googleMapsApiKey={API_KEY} libraries={libraries}>
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+        onCenterChanged
+        onZoomChanged
+        clickableIcons={true}
+      >
+        {/* Child components, such as markers, info windows, etc. */}
+        {searchBox
+          ? searchBox.map(item => {
+              console.log(item.geometry.location.lat());
+              return (
+                <Marker
+                  key={item.place_id}
+                  position={{
+                    lat: item.geometry.location.lat(),
+                    lng: item.geometry.location.lng(),
+                  }}
+                />
+              );
+            })
+          : null}
+
+        <InfoWindow position={{ lat: 47.5, lng: -122.644 }}>
+          <div>
+            <h1>InfoWindow</h1>
+          </div>
+        </InfoWindow>
+        <StandaloneSearchBox onLoad={onLoad} onPlacesChanged={onPlacesChanged}>
+          <input
+            type='text'
+            placeholder='Search Bathrooms here'
+            style={{
+              boxSizing: `border-box`,
+              border: `1px solid transparent`,
+              width: `240px`,
+              height: `32px`,
+              padding: `0 12px`,
+              borderRadius: `3px`,
+              boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+              fontSize: `14px`,
+              outline: `none`,
+              textOverflow: `ellipses`,
+              position: 'absolute',
+              left: '50%',
+              marginLeft: '-120px',
+            }}
+          />
+        </StandaloneSearchBox>
+      </GoogleMap>
+    </LoadScript>
+  );
+}
+
+export default MyComponents;
