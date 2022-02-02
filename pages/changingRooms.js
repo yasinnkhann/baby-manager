ae79d1d4342397bbc5ea4945f199ab8c70770555
Alissa Luk <alissaluk1995@gmail.com>
2022-02-02 11:19:19 -0800

    implemented memory leak fix and css for current location button

--- pages/changingRooms.js
+++ pages/changingRooms.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import MyLocationIcon from '@mui/icons-material/MyLocation';
 import axios from 'axios';
 import Head from 'next/head';
-import Script from 'next/script';
 import MyComponent from '../components/ChangingRoom/GoogleMap.js';
 
 export default function ChangingRooms(props) {
@@ -11,20 +10,26 @@ export default function ChangingRooms(props) {
   const [home, setHome] = useState(false);
 
   useEffect(() => {
+    let isApiSubscribed = true;
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         position => {
-          const pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          };
-          setCurrentLoc(pos);
+          if (isApiSubscribed) {
+            const pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+            };
+            setCurrentLoc(pos);
+          }
         },
         () => {
           console.log('user denied permission');
         }
       );
     }
+    return () => {
+      isApiSubscribed = false;
+    };
   }, []);
 
   const clickHandler = () => {
@@ -45,10 +50,28 @@ export default function ChangingRooms(props) {
     }
   };
 
+  let buttonStyle = {
+    padding: '7px',
+    backgroundColor: 'white',
+    borderRadius: '2px',
+    fontFamily: 'Roboto, Arial, sans-serif',
+    boxShadow: 'rgb(0 0 0 / 30%) 0px 1px 4px -1px',
+    zIndex: '30',
+    position: 'absolute',
+    top: '80px',
+    right: '60px',
+    height: '40px',
+  };
+
   return (
-    <div style={{ marginTop: '100px' }}>
-      <MyLocationIcon onClick={clickHandler} />
-      <button onClick={clickHandler}>Current Location</button>
+    <div style={{ marginTop: '70px' }}>
+      <Head>
+        <title>Baby Manager | Changing Room Locator</title>
+      </Head>
+      <button style={buttonStyle} onClick={clickHandler}>
+        {' '}
+        <MyLocationIcon /> <div className='hidden md:inline'>Current Location</div>
+      </button>
       <MyComponent
         home={home}
         setHome={setHome}
