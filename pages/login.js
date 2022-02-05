623fb006bf48cfc976529befd783f5578788644f
Yasin Khan <yasinkhan500@gmail.com>
2022-02-04 20:37:17 -0800

    test

--- pages/login.js
+++ pages/login.js
@@ -112,63 +112,63 @@ export default function Login() {
   });
 
   return (
-    <ThemeProvider theme={theme}>
-      <div className='h-screen flex flex-col content-center font-["Rubik"]'>
-        <Head>
-          <title>BabyManager | Login</title>
-        </Head>
-        <div className='w-full max-w-xs m-auto bg-indigo-100/75 rounded p-5'>
-          <Button
-            onClick={googleSignIn}
-            className='w-full bg-indigo-700 hover:bg-pink-700 text-white font-bold py-2 px-4 mb-6 rounded'
-            variant='contained'
-          >
-            Login with Google
-          </Button>
+    // <ThemeProvider theme={theme}>
+    <div className='h-screen flex flex-col content-center font-["Rubik"]'>
+      <Head>
+        <title>BabyManager | Login</title>
+      </Head>
+      <div className='w-full max-w-xs m-auto bg-indigo-100/75 rounded p-5'>
+        <Button
+          onClick={googleSignIn}
+          className='w-full bg-indigo-700 hover:bg-pink-700 text-white font-bold py-2 px-4 mb-6 rounded'
+          variant='contained'
+        >
+          Login with Google
+        </Button>
 
-          <fieldset>
-            <form onSubmit={handleSubmit} className='font-["Rubik"]'>
-              <TextField
-                className='w-full mb-2 '
-                type='email'
-                color='primary'
-                id='email'
-                label='Email:'
-                name='email'
-                variant='filled'
-                onChange={handleChange}
-              ></TextField>
+        <fieldset>
+          <form onSubmit={handleSubmit} className='font-["Rubik"]'>
+            <TextField
+              className='w-full mb-2 '
+              type='email'
+              color='primary'
+              id='email'
+              label='Email:'
+              name='email'
+              variant='filled'
+              onChange={handleChange}
+            ></TextField>
 
-              <TextField
-                className='w-full mb-2 '
-                type='password'
-                color='primary'
-                id='password'
-                label='Password:'
-                name='password'
-                variant='filled'
-                onChange={handleChange}
-              ></TextField>
+            <TextField
+              className='w-full mb-2 '
+              type='password'
+              color='primary'
+              id='password'
+              label='Password:'
+              name='password'
+              variant='filled'
+              onChange={handleChange}
+            ></TextField>
 
-              <Button
-                className='w-full bg-indigo-700 hover:bg-pink-700 text-white font-bold py-2 px-4 mb-6 rounded'
-                type='submit'
-              >
-                Login
-              </Button>
-            </form>
-          </fieldset>
-
-          <div className='justify-self-center text-center'>
-            <button
-              className='text-blue-700 text-sm hover:text-pink-700'
-              onClick={goToRegisterPage}
+            <Button
+              className='w-full bg-indigo-700 hover:bg-pink-700 text-white font-bold py-2 px-4 mb-6 rounded'
+              type='submit'
             >
-              Don&apos;t have an account? Sign up here!
-            </button>
-          </div>
+              Login
+            </Button>
+          </form>
+        </fieldset>
+
+        <div className='justify-self-center text-center'>
+          <button
+            className='text-blue-700 text-sm hover:text-pink-700'
+            onClick={goToRegisterPage}
+          >
+            Don&apos;t have an account? Sign up here!
+          </button>
         </div>
       </div>
-    </ThemeProvider>
+    </div>
+    // </ThemeProvider>
   );
 }
